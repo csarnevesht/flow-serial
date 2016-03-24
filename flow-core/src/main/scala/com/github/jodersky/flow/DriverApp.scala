f40b62dbed1399e7de19a2d3114346bab53b74ea
csarnevesht@livongo.com <M13rd@123#>
2016-03-23 22:36:03 -0700

    add DriverApp

---
+++ flow-core/src/main/scala/com/github/jodersky/flow/DriverApp.scala
@@ -0,0 +1,17 @@
+package com.github.jodersky.flow
+
+object DriverApp extends App {
+
+  val serialManager = new SerialManager
+
+  val connection = serialManager.open()
+
+  serialManager.sendRequest(connection, "about")
+  val modemResponse = serialManager.getResponse(connection)
+
+  serialManager.sendRequest(connection, "help")
+  val downloadResponse = serialManager.getResponse(connection)
+
+  connection.close
+
+}
